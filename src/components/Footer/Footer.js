9817ccb6584714d8b073fc6c332b3426a21d5ac4
Orhun Özer <orhunozor@gmail.com>
2019-02-28 00:36:32 +0300

    Develop (#3)
    * Add utils function
    
    - Chunk function was repeated in several components, moved to shared js
    
    * Add generic API url for all components
    
    * Add github-pages
    
    * Change team logo sources to aws
    
    * Delete all logo files
    
    * Add gh-pages index redirect
    
    * Update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,8 +6,7 @@ const Footer = () => {
       <div className="content has-text-centered">
         <p>
           Powered by <a href="https://bulma.io"><strong>Bulma</strong></a>. The source code is licensed
-              <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content
-              is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+              <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>.
             </p>
       </div>
     </footer>
